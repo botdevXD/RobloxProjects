8546cbe4acbe8fe9d3bc45a6e9a2fbc14eb475bc
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-08-08 16:28:25 +0100

    Update InputController.lua

--- InputController.lua
+++ InputController.lua
@@ -1,6 +1,10 @@
-local InputController = {
-    LoadPosition = 0
+local UserInputService = game:GetService("UserInputService")
+local InputController = {}
+local Services = {
+    UserInputService = game:GetService("UserInputService"),
+    ContextActionService = game:GetService("ContextActionService")
 }
+
 InputController.__index = InputController
 
 function InputController.new()
@@ -8,6 +12,8 @@ function InputController.new()
     self.Functions = getmetatable(newproxy(true))
     self.Signals = getmetatable(newproxy(true))
     self.Connected = getmetatable(newproxy(true))
+    self.VarArgs = getmetatable(newproxy(true))
+    self.ServiceOrSignal = getmetatable(newproxy(true))
     return self
 end
 
@@ -15,26 +21,61 @@ function InputController:Add(Name, Function, ServiceOrSignal, ...)
     if type(Function) == "function" then
         if typeof(ServiceOrSignal) == "RBXScriptSignal" then
             -- InputBegan, InputEnded
-            
+
+            self.VarArgs[Name] = {...}
             self.Functions[Name] = Function
-            self.Signals[Name] = ServiceOrSignal:Connect(Function, ...)
+            self.ServiceOrSignal[Name] = ServiceOrSignal
+            self.Signals[Name] = ServiceOrSignal:Connect(Function, ...) or 9e9
             self.Connected[Name] = true
         elseif tostring(ServiceOrSignal) == "ContextActionService" then
             -- BindAction
 
+            self.VarArgs[Name] = {...}
             self.Functions[Name] = Function
-            self.Signals[Name] = ServiceOrSignal:BindAction(Name, Function, ...)
+            self.ServiceOrSignal[Name] = ServiceOrSignal
+            self.Signals[Name] = ServiceOrSignal:BindAction(Name, Function, ...) or 9e9
             self.Connected[Name] = true
         end
     end
+
+    return self
 end
 
 function InputController:Enable()
-    
+    for FunctionName, Function in pairs(self.Functions) do
+        if self.Connected[FunctionName] == false then
+            local VarArgs = self.VarArgs[FunctionName]
+            local ServiceOrSignal = self.ServiceOrSignal[FunctionName]
+
+            if typeof(ServiceOrSignal) == "RBXScriptSignal" then
+                self.Signals[FunctionName] = ServiceOrSignal:Connect(Function, VarArgs and unpack(VarArgs) or unpack({})) or 9e9
+                self.Connected[FunctionName] = true
+            elseif tostring(ServiceOrSignal) == "ContextActionService" then
+                self.Signals[FunctionName] = Services.ContextActionService:BindAction(FunctionName, Function, VarArgs and unpack(VarArgs) or unpack({})) or 9e9
+                self.Connected[FunctionName] = true
+            end
+        end
+    end
+
+    return self
 end
 
 function InputController:Disable()
-    
+    for FunctionName, Signal in pairs(self.Signals) do
+        if typeof(Signal) == "RBXScriptConnection" then
+            Signal:Disconnect()
+        else
+            pcall(function()
+                Services.ContextActionService:UnbindAction(FunctionName)
+            end)
+        end
+
+        self.Connected[FunctionName] = false
+    end
+
+    table.clear(self.Signals)
+
+    return self
 end
 
 return InputController
