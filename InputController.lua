f2a65816a2beb8d1e7dff329f150838bf1a3ca7b
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-08-08 16:05:51 +0100

    Update InputController.lua

--- InputController.lua
+++ InputController.lua
@@ -7,6 +7,7 @@ function InputController.new()
     local self = setmetatable({}, InputController)
     self.Functions = getmetatable(newproxy(true))
     self.Signals = getmetatable(newproxy(true))
+    self.Connected = getmetatable(newproxy(true))
     return self
 end
 
@@ -14,12 +15,16 @@ function InputController:Add(Name, Function, ServiceOrSignal, ...)
     if type(Function) == "function" then
         if typeof(ServiceOrSignal) == "RBXScriptSignal" then
             -- InputBegan, InputEnded
+            
             self.Functions[Name] = Function
             self.Signals[Name] = ServiceOrSignal:Connect(Function, ...)
+            self.Connected[Name] = true
         elseif tostring(ServiceOrSignal) == "ContextActionService" then
             -- BindAction
+
             self.Functions[Name] = Function
             self.Signals[Name] = ServiceOrSignal:BindAction(Name, Function, ...)
+            self.Connected[Name] = true
         end
     end
 end
