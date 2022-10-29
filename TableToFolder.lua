4d7598cdef76fdf3d7ad47abc062fd43c45e7378
_Ben <54240316+botdevXD@users.noreply.github.com>
2022-10-29 12:52:10 +0100

    Update TableToFolder.lua

--- TableToFolder.lua
+++ TableToFolder.lua
@@ -1,7 +1,5 @@
 -- Made by _Ben#7494 / (0x74_Dev, UserId = 152711071)
 
--- Made by _Ben#7494 / (0x74_Dev, UserId = 152711071)
-
 local Types = {
 	["string"] = "StringValue",
 	["cframe"] = "CFrameValue",
@@ -26,37 +24,6 @@ local function Convert(Object, TableName, Parent)
 			else
 				local ValueType = Types[tostring(typeof(Value)):lower()]
 
-				if ValueType ~= nil then
-					local ValueInstance = Instance.new(ValueType, Folder)
-					ValueInstance.Name = Index
-					ValueInstance.Value = Value
-				end
-			end
-		end
-		
-		return Folder
-	end
-end
-
-return function(Table : table, FolderName : string, Parent : Instance)
-	local Converted = Convert(Table, FolderName, Parent)
-end
-
-local function Convert(Object, TableName, Parent)
-	if type(Object) == "table" then
-		local Folder = Instance.new("Folder", Parent)
-		Folder.Name = TableName or "UnknownName"
-		
-		for Index, Value in pairs(Object) do
-			if type(Value) == "table" then
-				local TableFolder = Convert(Value, tostring(Index), Folder)
-				
-				if TableFolder ~= nil then
-					TableFolder.Parent = Folder
-				end
-			else
-				local ValueType = Types[tostring(typeof(Value)):lower()]
-				
 				if ValueType ~= nil then
 					local ValueInstance = Instance.new(ValueType, Folder)
 					ValueInstance.Name = Index
